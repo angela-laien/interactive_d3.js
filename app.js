54d8340f04f2f8af43f0a08aea7f7c3d3558049f
angela-laien <laienxie@gmail.com>
2021-04-01 14:01:56 -0700

    data setup done

---
+++ app.js
@@ -0,0 +1,6 @@
+const ANTIOXIDANTS_DATA = [
+    { id: 'd1', fruit: 'Blueberries', value: 9 },
+    { id: 'd2', fruit: 'Rasberries', value: 4 },
+    { id: 'd3', fruit: 'Plums', value: 3 },
+    { id: 'd4', fruit: 'Strawberries', value: 2 },
+]
